a1bd3303b0d39232a3e8e1fa80c59730d5758e1a
Cristian <cristian.setto@gmail.com>
2026-08-19 00:40:22 -0400

    Fix: Inyectar definicion global CFG_TUD_CDC_EP_BUFSIZE para resolver TinyUSB

--- dispositivo/firmware_personalizado/modulo_pdm/micropython.cmake
+++ dispositivo/firmware_personalizado/modulo_pdm/micropython.cmake
@@ -1,5 +1,8 @@
 add_library(usermod_pdm INTERFACE)
 
+# Solución al error de TinyUSB de ESP-IDF para ESP32-S3
+add_compile_definitions(CFG_TUD_CDC_EP_BUFSIZE=512)
+
 target_sources(usermod_pdm INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modulo_pdm.c
 )
